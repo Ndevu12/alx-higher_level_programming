279d16a6569d2ce7c8b1acda1ef1f0ce15aac2b2
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-11-30 15:22:25 -0800

    updates

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
-from variable_load_5 import a as number
-print(number)
+#!/usr/bin/python
+
+if __name__ == "__main__":
+    """Print the value of variable a from variable_load_5."""
+    from variable_load_5 import a 
+    print(a)
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,7 @@
+# module and import
+
+# THIS IS A time for getting fun with module and import
+
+don't forget to talk about command line arguments
+
+we are going to deal with 6 to ten tasks
