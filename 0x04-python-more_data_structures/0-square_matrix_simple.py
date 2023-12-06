939197781c086d58511ff937dc333a11f83a2e3f
Ndevu <niyokwizerwajeanpaulelis@gmail.com>
2023-12-06 12:09:42 -0800

    removing errors

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def spuare_matrix_simple(matrix=[]):
-    return ([list(map(lambda x: x ** 2, i))for i in matrix])
+    new = matrix.copy()
+    for i in range(len(matrix)):
+        new[i] = list(map(lambda x: x**2, matrix[i]))
+    return (new)
